5c04ed0b4ae93fc5f8b75bfafd2961fd44d8bb6a
suskz <ielsondias1@gmail.com>
2024-05-31 20:10:00 -0300

    1003

--- problems/1001.cs
+++ problems/1001.cs
@@ -6,7 +6,6 @@ The input file will contain 2 integer numbers.
 Output
 Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.*/
 
-
 /*int A = Convert.ToInt32(Console.ReadLine());
 int B = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("X = " + (A+B));*/
