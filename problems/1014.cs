f24472c115816d874d9e388098ec71bbf820c281
suskz <ielsondias1@gmail.com>
2024-06-08 19:11:54 -0300

    1014

---
+++ problems/1014.cs
@@ -0,0 +1,14 @@
+/*Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
+
+Input
+The input file contains two values: one integer value X representing the total distance (in Km) and the second one is a floating point number Y  representing the spent fuel total, with a digit after the decimal point.
+
+Output
+Present a value that represents the average consumption of a car with 3 digits after the decimal point, followed by the message "km/l".*/
+
+/*using System.Globalization;
+
+int X = Convert.ToInt32(Console.ReadLine());
+double Y = Convert.ToDouble(Console.ReadLine(), CultureInfo.CreateSpecificCulture("en-US"));
+double avg = X/Y;
+Console.WriteLine(avg.ToString("F3", CultureInfo.CreateSpecificCulture("en-US")) + " km/l");*/
