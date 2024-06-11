4d092908045499f5b15b016d8b1a06543f3c65cd
suskz <ielsondias1@gmail.com>
2024-06-11 14:54:59 -0300

    1017

--- obj/Debug/net8.0/beecrowd.AssemblyInfo.cs
+++ obj/Debug/net8.0/beecrowd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beecrowd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e84ab2fbcbff15dbc422bb227af57fd399be74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b526ee1921142b1c200da5d9aa38cfce8d2a893a")]
 [assembly: System.Reflection.AssemblyProductAttribute("beecrowd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beecrowd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
