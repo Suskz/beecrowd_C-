e1d201488c1b876607dc871cfbec6dc82c5d05c0
suskz <ielsondias1@gmail.com>
2024-06-12 12:21:02 -0300

    1019

---
+++ problems/1019.cs
@@ -0,0 +1,14 @@
+/*Read an integer value, which is the duration in seconds of a certain event in a factory, and inform it expressed in hours:minutes:seconds.
+
+Input
+The input file contains an integer N.
+
+Output
+Print the read time in the input file (seconds) converted in hours:minutes:seconds like the following example.*/
+
+/*int time = Convert.ToInt32(Console.ReadLine());
+int sec = time % 60;
+int min = time / 60;
+min = min % 60;
+int hours = time / 3600;
+Console.WriteLine(hours + ":" + min + ":" + sec);*/
