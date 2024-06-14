da06fcd9eebc7b58d681e370018f6327c51edd49
suskz <ielsondias1@gmail.com>
2024-06-14 20:01:07 -0300

    1018

--- problems/1018.cs
+++ problems/1018.cs
@@ -7,25 +7,11 @@ Output
 Print the read number and the minimum quantity of each necessary banknotes in Portuguese language, as the given example. Do not forget to print the end of line after each line, otherwise you will receive “Presentation Error”.*/
 
 /*int val = Convert.ToInt32(Console.ReadLine());
-int val100 = val / 100;
-int restval = val % 100;
-int val50 = restval / 50;
-restval = restval % 50;
-int val20 = restval / 20;
-restval = restval % 20;
-int val10 = restval / 10;
-restval = restval % 10;
-int val5 = restval / 5;
-restval = restval % 5;
-int val2 = restval / 2;
-restval = restval % 2;
-int val1 = restval / 1;
+int[] nota = {100, 50, 20, 10, 5, 2, 1};
+int qtdnota;
 
-Console.WriteLine(val);
-Console.WriteLine(val100 + " nota(s) de R$ 100,00");
-Console.WriteLine(val50 + " nota(s) de R$ 50,00");
-Console.WriteLine(val20 + " nota(s) de R$ 20,00");
-Console.WriteLine(val10 + " nota(s) de R$ 10,00");
-Console.WriteLine(val5 + " nota(s) de R$ 5,00");
-Console.WriteLine(val2 + " nota(s) de R$ 2,00");
-Console.WriteLine(val1 + " nota(s) de R$ 1,00");*/
+for (int i = 0; i < nota.Length; i++){
+    qtdnota = val / nota[i];
+    Console.WriteLine(qtdnota + " nota(s) de R$ " + nota[i].ToString("F2"));
+    val -= qtdnota * nota[i]; 
+}*/
