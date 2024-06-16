601b82f19bed4b51cccf05d41c763327b12ab128
suskz <ielsondias1@gmail.com>
2024-06-15 21:42:55 -0300

    1035

---
+++ problems/1035.cs
@@ -0,0 +1,18 @@
+/*Read 4 integer values A, B, C and D. Then if B is greater than C and D is greater than A and if the sum of C and D is greater than the sum of A and B and if C and D were positives values and if A is even, write the message “Valores aceitos” (Accepted values). Otherwise, write the message “Valores nao aceitos” (Values not accepted).
+
+Input
+Four integer numbers A, B, C and D.
+
+Output
+Show the corresponding message after the validation of the values​​.*/
+
+/*int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int D = Convert.ToInt32(Console.ReadLine());
+
+if ((B > C) & (D > A) & (C+D > A+B) & (C >= 0) & (D >= 0) & (A % 2 == 0)){
+    Console.WriteLine("Valores aceitos");
+}else{
+    Console.WriteLine("Valores não aceitos");
+}*/
